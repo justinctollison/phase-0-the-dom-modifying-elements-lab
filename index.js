5068114f45250ce3f6e67a2c4af6bba50173f66e
justinctollison <justinctollison@gmail.com>
2021-04-12 18:32:57 -0700

    Completed Assigment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+const body = document.getElementById('main');
+body.remove();
+
+const newHeader = document.createElement('h1')
+document.body.appendChild(newHeader);
+
+newHeader.id = "victory"
+newHeader.innerHTML = "YOUR-NAME is the champion"
+
+const h1 = document.createElement('victory')
+h1.innerHTML = "victory"
+
+newHeader.appendChild(h1);
